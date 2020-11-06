6edeef99c9d224630b93002a2554b62c807ad0ad
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2020-11-06 18:52:42 +0900

    headerBackTitleVisible 값 true -> false로 변경

--- src/components/Screens/TabScreen/Home.tsx
+++ src/components/Screens/TabScreen/Home.tsx
@@ -25,6 +25,7 @@ export default function Home() {
             backgroundColor: '#F9F9F9',
             height: '100%',
           },
+          headerBackTitleVisible: false,
         }}
       />
       <Stack.Screen
@@ -35,6 +36,7 @@ export default function Home() {
             backgroundColor: '#F9F9F9',
             height: '100%',
           },
+          headerBackTitleVisible: false,
         }}
       />
       <Stack.Screen
@@ -45,6 +47,7 @@ export default function Home() {
             backgroundColor: '#F9F9F9',
             height: '100%',
           },
+          headerBackTitleVisible: false,
         }}
       />
       <Stack.Screen
@@ -55,6 +58,7 @@ export default function Home() {
             backgroundColor: '#F9F9F9',
             height: '100%',
           },
+          headerBackTitleVisible: false,
         }}
       />
       <Stack.Screen
