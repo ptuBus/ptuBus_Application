b31911025c2acb38db3dc71d4ae7b5fa2be6cac8
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2020-11-11 22:34:28 +0900

    Index ; index.tsx 삭제

--- src/Main/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as HomeScreen } from './Home/Screen/HomeScreen';
-export { default as FavoriteScreen } from './Favorite/Screen/FavoriteScreen';
-export { default as HomeNavigation } from './Home/Navigation/HomeNavigation';
--- src/Menu/SchoolBus/index.tsx
+++
@@ -1,3 +0,0 @@
-export {default as SchoolBusUpScreen} from './Screens/SchoolBusUpScreen';
-export {default as SchoolBusDownScreen} from './Screens/SchoolBusDownScreen';
-export {default as SchoolBusNavigation} from './Navigation/SchoolBusNavigation';
