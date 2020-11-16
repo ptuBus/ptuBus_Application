08a9e9b019f2cf775a28c8308ac530257d34bf6c
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2020-11-16 19:17:51 +0900

    Train : 기차 네비게이션, 스크린 추가

---
+++ src/Menu/Train/Navigation/TrainNavigation.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {createStackNavigator} from '@react-navigation/stack'
+import TrainScreen from "../Screens/TrainScreen";
+import TrainTimeTableScreen from "../Screens/TrainTimeTableScreen";
+
+const Stack = createStackNavigator();
+
+export default function TrainStackNavigation() {
+    return (
+        <Stack.Navigator screenOptions={{headerBackTitleVisible: false}}>
+            <Stack.Screen
+                name="기차"
+                component={TrainScreen}
+                options={{
+                    headerStyle: {
+                        backgroundColor: '#F9F9F9',
+                    },
+                }}
+            />
+            <Stack.Screen
+                name="기차시간표"
+                component={TrainTimeTableScreen}
+                options={{
+                    headerStyle: {
+                        backgroundColor: '#F9F9F9',
+                    },
+                    title: '기차',
+                }}
+            />
+        </Stack.Navigator>
+    );
+}
---
+++ src/Menu/Train/Screens/TrainScreen.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+import ArrivalButton from "../../../Components/Buttons/ArrivalButton";
+import InquiryButton from "../../../Components/Buttons/InquiryButton";
+
+export default function TrainScreen({navigation}) {
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.card}>
+                <View style={styles.selectDest}>
+                    <ArrivalButton
+                        title={'평택'}
+                    />
+                    <ArrivalButton
+                        title={'서울'}
+                    />
+                </View>
+                <View style={styles.selectTime}>
+
+                </View>
+                <View style={styles.selectWeek}>
+
+                </View>
+                <View style={styles.InquiryBtnSector}>
+                    <InquiryButton
+                        onPress={() => navigation.navigate('기차시간표')}
+                        width={'95%'}
+                        height={'55%'}
+                    />
+                </View>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10,
+        backgroundColor: '#FCFCFC',
+        alignItems: 'center',
+    },
+    card: {
+        backgroundColor: '#fff',
+        borderColor: '#AAAAAA',
+        borderRadius: 10,
+        borderWidth: 0.5,
+        width: '85%',
+        height: '70%',
+        top: '14%',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+    selectDest: {
+        backgroundColor: '#FFF',
+        width: '100%',
+        flex: 3,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+    },
+    selectTime: {
+        backgroundColor: '#FFF',
+        width: '100%',
+        flex: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: '#AAAAAA',
+        borderTopWidth: 0.5,
+    },
+    selectWeek: {
+        backgroundColor: '#FFF',
+        width: '100%',
+        flex: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: '#AAAAAA',
+        borderTopWidth: 0.5,
+    },
+    InquiryBtnSector: {
+        backgroundColor: '#FFFFFF',
+        width: '100%',
+        flex: 1.3,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: '#AAAAAA',
+        borderTopWidth: 0.5,
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10,
+    },
+});
